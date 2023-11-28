55c624e5eccf93d73d6054cd756732c7afd3d924
M-Laetitia <molin.laetitia@gmail.com>
2023-11-28 17:44:15 +0100

    Feat: create avatar form type

---
+++ src/Form/AvatarType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class AvatarType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('avatar',FileType::class, [
+                'required' => false,
+                'label' => 'Profile picture :',
+                'data_class' => null,
+
+                // make it optional so you don't have to re-upload the PDF file
+                // every time you edit the Product details
+                'required' => false,
+
+                // unmapped fields can't define their validation using annotations
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'extensions' => [
+                            'png',
+                            'jpeg',
+                            'jpg',
+                        ],
+                        'extensionsMessage' => 'Please upload a valid image (autorized files: png, jpeg, jpg - max size : 1024k)',
+                    ])
+                ],
+
+            ])
+            
+            ->add('Save', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}

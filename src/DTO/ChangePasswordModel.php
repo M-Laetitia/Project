20516e6811ebf2cbfb1e8a89998a0e6affa13efd
M-Laetitia <molin.laetitia@gmail.com>
2023-11-29 13:48:57 +0100

    Feat: create DTO for password change

---
+++ src/DTO/ChangePasswordModel.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DTO;
+
+class ChangePasswordModel
+{
+
+    private $oldPassword;
+    private $newPassword;
+
+    public function getOldPassword(): ?string
+    {
+        return $this->oldPassword;
+    }
+
+    public function setOldPassword(string $oldPassword): self
+    {
+        $this->oldPassword = $oldPassword;
+        return $this;
+    }
+
+    public function getNewPassword(): ?string
+    {
+        return $this->newPassword;
+    }
+
+    public function setNewPassword(string $newPassword): self
+    {
+        $this->newPassword = $newPassword;
+        return $this;
+    }
+}

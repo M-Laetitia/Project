ccafbddca32edf7ae0f64850b2b797f2dc9f4d5d
M-Laetitia <molin.laetitia@gmail.com>
2023-11-29 15:05:28 +0100

    Feat: create ArtistController

---
+++ src/Controller/ArtistController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArtistController extends AbstractController
+{
+    #[Route('/artist', name: 'app_artist')]
+    public function index(): Response
+    {
+        return $this->render('artist/index.html.twig', [
+            'controller_name' => 'ArtistController',
+        ]);
+    }
+}

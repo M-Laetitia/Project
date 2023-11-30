4075bb653e6f83137693246936daa7b4ddfc3516
M-Laetitia <molin.laetitia@gmail.com>
2023-11-30 14:26:14 +0100

    Feat: add picture entity

---
+++ src/Entity/Picture.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PictureRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PictureRepository::class)]
+class Picture
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $url = null;
+
+    #[ORM\Column(length: 150)]
+    private ?string $altDescription = null;
+
+    #[ORM\ManyToOne(inversedBy: 'pictures')]
+    private ?User $user = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(string $url): static
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    public function getAltDescription(): ?string
+    {
+        return $this->altDescription;
+    }
+
+    public function setAltDescription(string $altDescription): static
+    {
+        $this->altDescription = $altDescription;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}

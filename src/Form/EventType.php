f49be4586e4dbcc2bc52299affa65f6a43ae2d07
M-Laetitia <molin.laetitia@gmail.com>
2023-12-01 13:36:25 +0100

    Feat: add eventType form

---
+++ src/Form/EventType.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Event;
+use App\Entity\CategoryEvent;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Validator\Constraints\GreaterThan;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class EventType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('status', ChoiceType::class, [
+                'choices' => [
+                    'open' => 'open',
+                    'close' => 'close',
+                    'open soon' => 'open_soon',
+                    'archived' => 'archived',
+                ]
+            ])
+            ->add('type', ChoiceType::class, [
+                'choices' => [
+                    'private' => 'private',
+                    'public' => 'close',
+                ]
+            ])
+
+
+            ->add('nbPlace', IntegerType::class, [
+                'constraints' => [
+                    new GreaterThan([
+                        'value' => 0,
+                        'message' => 'The number of place must be a number greater than zero.'
+                    ]),
+                ],
+                'attr' => ['min' => 0],
+            ])
+
+            
+            ->add('categoryEvent', EntityType::class, [
+                'class' => CategoryEvent::class,
+                // 'attr' => ['class' => 'custom-dropdown'],
+            ])
+
+            ->add('Create', SubmitType::class);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Event::class,
+        ]);
+    }
+}

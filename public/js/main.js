6db615e94cb151ceed96b3e974c87abe258dcae3
M-Laetitia <molin.laetitia@gmail.com>
2023-12-03 18:28:52 +0100

    Feat: make disappear the flash message after 5s

---
+++ public/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    // Disappear after 5s (5000 millisecondes)
+    setTimeout(function() {
+        $('.alert').slideUp(1000); // 1000 millisecondes for the animation's duration
+    }, 5000);
+});

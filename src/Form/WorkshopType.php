3c98aac7c4bf850419233c0d5f011190711ac441
M-Laetitia <molin.laetitia@gmail.com>
2023-12-05 13:55:42 +0100

    Feat: create Workshop form

---
+++ src/Form/WorkshopType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Workshop;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class WorkshopType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('startDate')
+            ->add('endDate')
+            ->add('nbRooms')
+            ->add('picture')
+            ->add('user')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Workshop::class,
+        ]);
+    }
+}

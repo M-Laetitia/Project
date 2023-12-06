a70cb90fa93668659bf2bdeeeb57ab494305a497
M-Laetitia <molin.laetitia@gmail.com>
2023-12-06 11:09:22 +0100

    Feat: create new/edit workshop

--- src/Controller/WorkshopController.php
+++ src/Controller/WorkshopController.php
@@ -3,7 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Workshop;
+use App\Form\WorkshopType;
+use App\Repository\UserRepository;
 use App\Repository\WorkshopRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -28,6 +32,42 @@ class WorkshopController extends AbstractController
 
         return $this->render('workshop/show.html.twig', [
             'workshop' => $workshop,
+        ]);
+    }
+
+   
+    #[Route('/dashboard/new', name:'new_workshop')]
+    #[Route('/dashboard/{id}/edit', name:'edit_workshop')]
+    public function new_edit(Workshop $workshop = null, Request $request, UserRepository $userRepository, EntityManagerInterface $entityManager ) : Response
+    {
+
+        // Fetch users with the specified role
+        $users = $userRepository->findUsersbyRole("ROLE_SUPERVISOR");
+        
+        // dump($users);die;
+        if(!$workshop) {
+            $workshop = new Workshop();
+        }
+
+        $form = $this->createForm(WorkshopType::class, $workshop, [
+            'users' => $users,
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid() ) {
+            $workshop = $form->getData();
+            $entityManager->persist($workshop);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_dashboard');
+        }
+
+    
+        return $this->render('dashboard/newWorkshop.html.twig', [
+            'formAddWorkshop' => $form,
+            'edit' =>$workshop->getId(),
+           
         ]);
     }
 

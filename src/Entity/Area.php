52d9ae3fdee5dd9ba6accc8ecef40afebc742c12
M-Laetitia <molin.laetitia@gmail.com>
2023-12-06 16:33:17 +0100

    Feat: create Area Entity

---
+++ src/Entity/Area.php
@@ -0,0 +1,185 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AreaRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AreaRepository::class)]
+class Area
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $detail = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $startDate = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $endDate = null;
+
+    #[ORM\Column]
+    private ?int $nbRooms = null;
+
+    #[ORM\Column(length: 30)]
+    private ?string $type = null;
+
+    #[ORM\Column(length: 30)]
+    private ?string $status = null;
+
+    #[ORM\OneToMany(mappedBy: 'area', targetEntity: ExpositionProposal::class)]
+    private Collection $expositionProposals;
+
+
+
+    public function __construct()
+    {
+        $this->expositionProposals = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getDetail(): ?string
+    {
+        return $this->detail;
+    }
+
+    public function setDetail(?string $detail): static
+    {
+        $this->detail = $detail;
+
+        return $this;
+    }
+
+    public function getStartDate(): ?\DateTimeInterface
+    {
+        return $this->startDate;
+    }
+
+    public function setStartDate(\DateTimeInterface $startDate): static
+    {
+        $this->startDate = $startDate;
+
+        return $this;
+    }
+
+    public function getEndDate(): ?\DateTimeInterface
+    {
+        return $this->endDate;
+    }
+
+    public function setEndDate(\DateTimeInterface $endDate): static
+    {
+        $this->endDate = $endDate;
+
+        return $this;
+    }
+
+    public function getNbRooms(): ?int
+    {
+        return $this->nbRooms;
+    }
+
+    public function setNbRooms(int $nbRooms): static
+    {
+        $this->nbRooms = $nbRooms;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, ExpositionProposal>
+     */
+    public function getExpositionProposals(): Collection
+    {
+        return $this->expositionProposals;
+    }
+
+    public function addExpositionProposal(ExpositionProposal $expositionProposal): static
+    {
+        if (!$this->expositionProposals->contains($expositionProposal)) {
+            $this->expositionProposals->add($expositionProposal);
+            $expositionProposal->setArea($this);
+        }
+
+        return $this;
+    }
+
+    public function removeExpositionProposal(ExpositionProposal $expositionProposal): static
+    {
+        if ($this->expositionProposals->removeElement($expositionProposal)) {
+            // set the owning side to null (unless already changed)
+            if ($expositionProposal->getArea() === $this) {
+                $expositionProposal->setArea(null);
+            }
+        }
+
+        return $this;
+    }
+
+   
+}

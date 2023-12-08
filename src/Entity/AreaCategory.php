4a011463de4426c55680c9ed6e386381d8d0b2d5
M-Laetitia <molin.Laetitia@gmail.com>
2023-12-08 16:16:25 +0100

    Feat: add AreaCategory entity

---
+++ src/Entity/AreaCategory.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AreaCategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AreaCategoryRepository::class)]
+class AreaCategory
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 150)]
+    private ?string $name = null;
+
+    #[ORM\ManyToMany(targetEntity: Area::class, mappedBy: 'areaCategories')]
+    private Collection $areas;
+
+    public function __construct()
+    {
+        $this->areas = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Area>
+     */
+    public function getAreas(): Collection
+    {
+        return $this->areas;
+    }
+
+    public function addArea(Area $area): static
+    {
+        if (!$this->areas->contains($area)) {
+            $this->areas->add($area);
+            $area->addAreaCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeArea(Area $area): static
+    {
+        if ($this->areas->removeElement($area)) {
+            $area->removeAreaCategory($this);
+        }
+
+        return $this;
+    }
+}

f88c9754a76ae9fe79de43f12be3f545f571a2ad
M-Laetitia <molin.Laetitia@gmail.com>
2023-12-08 09:30:06 +0100

    Refactor: get the area entity

--- src/Controller/ParticipationController.php
+++ src/Controller/ParticipationController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Area;
 use App\Entity\User;
 use App\Service\MailerService;
 use App\Entity\AreaParticipation;
+use App\Repository\AreaRepository;
+use App\Form\AreaParticipationType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Security;
@@ -24,12 +27,16 @@ class ParticipationController extends AbstractController
 
     // ^ Make a participation for an exposition
     #[Route('/expostion/{id}/new', name: 'new_exposition_participation')]
-    public function new(AreaParticipation $areaParticipation = null, User $user, Security $security, Request $request, EntityManagerInterface $entityManager, MailerService $mailerService) :Response
+    public function new(AreaParticipation $areaParticipation = null, Area $area, AreaRepository $areaRepository, Security $security, Request $request, EntityManagerInterface $entityManager, MailerService $mailerService) :Response
     {
 
+        // $area = $areaParticipation->
         $user = $security->getUser();
-
-
+        $areaId = $area->getId();
+        $area = $areaRepository->findBy(['id' => $areaId ]);
+        // dump($area); die;
+        // dump($area[0]); die;
+        
         $form = $this->createForm(AreaParticipationType::class);
         $form->handleRequest($request);
 
@@ -38,12 +45,12 @@ class ParticipationController extends AbstractController
             $expoParticipation = $form->getData();
             $expoParticipation->setInscriptionDate(new \DateTimeImmutable());
             $expoParticipation->setUser($user);
-            // $expoParticipation->setArea($id);
+            $expoParticipation->setArea($area[0]);
 
-            $entityManager->persis($expoParticipation);
+            $entityManager->persist($expoParticipation);
             $entityManager->flush();
 
-            //send the email
+            // //send the email
             $userEmail = $user->getEmail();
             $expositionDetails = 'test';
             $mailerService->sendExpositionProposalConfirmation($userEmail, $expositionDetails);
@@ -55,6 +62,7 @@ class ParticipationController extends AbstractController
 
         return $this->render('exposition/newParticipation.html.twig', [
             'formSendParticipation' => $form,
+            'user' => $user,
 
         ]);
     }

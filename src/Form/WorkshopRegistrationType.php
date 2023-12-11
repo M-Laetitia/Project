60bd443fa507c4c8756de63e189359c2266f509a
M-Laetitia <molin.laetitia@gmail.com>
2023-12-11 16:24:34 +0100

    Refactor: add type for input

--- src/Form/WorkshopRegistrationType.php
+++ src/Form/WorkshopRegistrationType.php
@@ -6,6 +6,7 @@ use App\Entity\WorkshopRegistration;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class WorkshopRegistrationType extends AbstractType
@@ -13,12 +14,11 @@ class WorkshopRegistrationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstname')
-            ->add('lastname')
+            ->add('firstname', TextType::class)
+            ->add('lastname', TextType::class)
             // ->add('registrationDate')
             // ->add('workshop')
             // ->add('user')
-
             ->add('Participate', SubmitType::class)
         ;
     }

6fb984ac3c97cd663a11065ae7123b7174e38b46
M-Laetitia <molin.laetitia@gmail.com>
2023-12-11 16:58:36 +0100

    Feat: create studio entity

---
+++ src/Entity/Studio.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StudioRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: StudioRepository::class)]
+class Studio
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $detail = null;
+
+    #[ORM\Column]
+    private ?int $nbRooms = null;
+
+    #[ORM\OneToMany(mappedBy: 'studio', targetEntity: Timeslot::class)]
+    private Collection $timeslots;
+
+    public function __construct()
+    {
+        $this->timeslots = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getDetail(): ?string
+    {
+        return $this->detail;
+    }
+
+    public function setDetail(?string $detail): static
+    {
+        $this->detail = $detail;
+
+        return $this;
+    }
+
+    public function getNbRooms(): ?int
+    {
+        return $this->nbRooms;
+    }
+
+    public function setNbRooms(int $nbRooms): static
+    {
+        $this->nbRooms = $nbRooms;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Timeslot>
+     */
+    public function getTimeslots(): Collection
+    {
+        return $this->timeslots;
+    }
+
+    public function addTimeslot(Timeslot $timeslot): static
+    {
+        if (!$this->timeslots->contains($timeslot)) {
+            $this->timeslots->add($timeslot);
+            $timeslot->setStudio($this);
+        }
+
+        return $this;
+    }
+
+    public function removeTimeslot(Timeslot $timeslot): static
+    {
+        if ($this->timeslots->removeElement($timeslot)) {
+            // set the owning side to null (unless already changed)
+            if ($timeslot->getStudio() === $this) {
+                $timeslot->setStudio(null);
+            }
+        }
+
+        return $this;
+    }
+}

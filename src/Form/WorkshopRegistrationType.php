06cac2112bafe6ade4c57c9fd9db78e055c9954a
M-Laetitia <molin.laetitia@gmail.com>
2023-12-11 11:53:12 +0100

    Feat: add workshopRegistrationType form

---
+++ src/Form/WorkshopRegistrationType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\WorkshopRegistration;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class WorkshopRegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname')
+            ->add('lastname')
+            // ->add('registrationDate')
+            // ->add('workshop')
+            // ->add('user')
+
+            ->add('Participate', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => WorkshopRegistration::class,
+        ]);
+    }
+}

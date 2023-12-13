1aed154f6dd8bad11da1872a4f1f8c1cdc72df71
M-Laetitia <molin.laetitia@gmail.com>
2023-12-13 14:59:12 +0100

    Feat: show list studios (dashboard admin)

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\AreaRepository;
+use App\Repository\StudioRepository;
 use App\Repository\WorkshopRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,20 +12,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'app_dashboard')]
-    public function index(WorkshopRepository $workshopRepository, AreaRepository $areaRepository): Response
+    public function index(WorkshopRepository $workshopRepository, AreaRepository $areaRepository, StudioRepository $studioRepository): Response
     {
 
         // $events = $eventRepository->findBy([]);
         $workshops = $workshopRepository->findBy([]);
         $events = $areaRepository->findBy(['type' => 'EVENT']);
         $expositions = $areaRepository->findBy(['type' => 'EXPO']);
-
+        $studios = $studioRepository->findBy([]);
 
         return $this->render('dashboard/index.html.twig', [
             // 'controller_name' => 'DashboardController',
             'events' => $events,
             'expositions' => $expositions,
-            'workshops' => $workshops
+            'workshops' => $workshops,
+            'studios' => $studios,
         ]);
     }
 }

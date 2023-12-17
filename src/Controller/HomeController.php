6cca9fe629989b6cab9e9ed920d843149bbbf58c
M-Laetitia <molin.laetitia@gmail.com>
2023-12-17 20:57:22 +0100

    Feat: API leaflet (wip)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,59 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use App\Repository\UserRepository;
+// use Geocoder\Query\GeocodeQuery;
+// use Geocoder\Provider\Nominatim\Nominatim;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
+
+    // private function getLatLngFromAddress(string $address): array
+    // {
+    //     // Utilize the geocoding function to obtain coordinates from the address
+    //     $geocoder = new Nominatim(new Client());
+
+    //     $result = $geocoder->geocodeQuery(GeocodeQuery::create($address));
+
+    //     if ($result->count() > 0) {
+    //         $latlng = $result->first()->getCoordinates();
+    //         return [$latlng->getLatitude(), $latlng->getLongitude()];
+    //     }
+
+    //     return [0, 0]; // Error handling, default coordinates
+    // }
+
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
+
+        $artists = $userRepository->findArtistUsers();
+
+        // $markers = [];
+
+        // foreach ($artists as $artist) {
+        //     $address = sprintf(
+        //         '%s, %s, %s',
+        //         $artist->getArtistInfos()['address']['street'],
+        //         $artist->getArtistInfos()['address']['city'],
+        //         $artist->getArtistInfos()['address']['postalCode']
+        //     );
+
+        //     $markers[] = [
+        //         'address' => $address,
+        //         'latlng' => $this->getLatLngFromAddress($address),
+        //     ];
+        // }
+
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'artists' => $artists,
+            // 'markers' => $markers,
         ]);
     }
+
 }

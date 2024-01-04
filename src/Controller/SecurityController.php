13d826c4fff6118b15ebd22cc0b5263e92c15386
M-Laetitia <molin.laetitia@gmail.com>
2024-01-04 23:04:24 +0100

    Feat : google Auth (wip)

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,10 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -20,13 +21,31 @@ class SecurityController extends AbstractController
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
+        $googleId = '346078539825-0uda84aeorj9gb7jindava1pdct869qm.apps.googleusercontent.com';
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername, 
+            'error' => $error,
+            'GOOGLE_ID' => $googleId,
+        ]);
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    // ^ auth google
+    #[Route(path: '/connect', name: 'app_connect')]
+    public function connect(Request $request): Response
+    {
+        $googleId = '346078539825-0uda84aeorj9gb7jindava1pdct869qm.apps.googleusercontent.com';
+        $code = $request->query->get('code');
+
+        return $this->render('security/connect.html.twig', [
+            'GOOGLE_ID' => $googleId,
+            'code' => $code,
+        ]);
     }
 }

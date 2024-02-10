05fd990834b92ce7d9c06cfd06a1b5f1283bc2d6
M-Laetitia <molin.laetitia@gmail.com>
2024-02-10 17:00:59 +0100

    Refactor: update the builder to use TimeSlotAvailability

--- src/Form/TimeSlotType.php
+++ src/Form/TimeSlotType.php
@@ -4,10 +4,12 @@ namespace App\Form;
 
 use App\Entity\Studio;
 use App\Entity\Timeslot;
+use App\Entity\TimeSlotAvailability;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
@@ -16,16 +18,31 @@ class TimeSlotType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('startDate',  DateTimeType::class, [
-                'widget' => 'single_text',
-                'label' => 'Starting date: * ',
+            // ->add('startDate',  DateTimeType::class, [
+            //     'widget' => 'single_text',
+            //     'label' => 'Starting date: * ',
+            //     'required' => true,
+            // ])
+
+            
+            // ->add('endDate',  DateTimeType::class, [
+            //     'widget' => 'single_text',
+            //     'label' => 'Ending date: * ',
+            //     'required' => true, 
+            // ])
+
+            ->add('day', DateType::class, [
+                'label' => 'Date',
+                'mapped' => false,
                 'required' => true, 
+                'label' => 'Day: ',
             ])
-            ->add('endDate',  DateTimeType::class, [
-                'widget' => 'single_text',
-                'label' => 'Ending date: * ',
+
+            ->add('TimeSlotAvailability', EntityType::class, [
                 'required' => true, 
+                'class' => TimeSlotAvailability::class,
             ])
+
 
             ->add('studio', EntityType::class, [
                 'required' => true, 
@@ -46,3 +63,4 @@ class TimeSlotType extends AbstractType
         ]);
     }
 }
+

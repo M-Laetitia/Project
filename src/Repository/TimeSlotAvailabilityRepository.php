7e44ab8cda4f2e3e53e292734397cd4d6429cd61
M-Laetitia <molin.laetitia@gmail.com>
2024-02-11 16:49:52 +0100

    Refactor: refined DQL: studio and date parameters only.

--- src/Repository/TimeSlotAvailabilityRepository.php
+++ src/Repository/TimeSlotAvailabilityRepository.php
@@ -34,7 +34,7 @@ class TimeSlotAvailabilityRepository extends ServiceEntityRepository
     //     );
 
 
-    public function findAvailableTimeSlots($studio, $startDate, $endDate )
+    public function findAvailableTimeSlots($studioId, $date )
     {
         $em = $this->getEntityManager();
         $qb = $em->createQueryBuilder();
@@ -47,13 +47,12 @@ class TimeSlotAvailabilityRepository extends ServiceEntityRepository
                 ->select('IDENTITY(ts.timeSlotAvailability)') // permet d'obtenir l'ID de l'entité TimeSlotAvailability liée à chaque entité TimeSlot, et permet de filtrer les TimeSlotAvailability en fonction des ID obtenus dans la sous-requête NOT IN.
                 ->from('App\Entity\TimeSlot', 'ts')
                 ->where('ts.studio = :studio')
-                ->andWhere('ts.endDate >= :startDate')
-                ->andWhere('ts.startDate <= :endDate')
+                ->andWhere('ts.date >= :date')
                 ->getDQL() //  retourne la chaîne de requête DQL générée par le qb. Cette chaîne de requête DQL est utilisée dans la sous-requête notIn() pour filtrer les TimeSlotAvailability. Cela permet de créer une requête DQL plus complexe et de l'utiliser comme condition dans une autre requête DQL.
         ))
-        ->setParameter('studio', $studio)
-        ->setParameter('startDate', $startDate)
-        ->setParameter('endDate', $endDate);
+        ->setParameter('studio', $studioId)
+        ->setParameter('date', $date);
+   
 
         $query = $qb->getQuery();
         return $query->getResult();

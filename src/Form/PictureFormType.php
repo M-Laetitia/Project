741ae6be11cce1d0ca42789a02a3f06b65f43cb4
M-Laetitia <molin.laetitia@gmail.com>
2024-02-14 23:38:45 +0100

    Refactor: add contraints for the image upload

--- src/Form/PictureFormType.php
+++ src/Form/PictureFormType.php
@@ -2,21 +2,71 @@
 
 namespace App\Form;
 
+use Assert\Image;
+use Assert\Length;
 use App\Entity\Picture;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class PictureFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('path')
-            ->add('altDescription')
-            ->add('type')
-            ->add('title')
-            ->add('user')
+
+          ->add('picture', FileType::class, [
+                'label' => false,
+                // 'mapped' => false,
+                'required' => true, 
+                'property_path' => 'path', // le champ "picture" dans le form est lié à la propriété "path" de l'entité Picture
+                'invalid_message' => 'The image must have a maximum width of 1800 and a minimum of 1600, a maximum height of 1000 and a minimum of 600, its size must not exceed 2M, and the accepted formats are png, jpeg, jpg, and webp.',
+                'constraints' => [
+                    new Image([
+                        'maxSize' => '2M', 
+                        'maxWidth' => 1800, 
+                        'maxHeight' => 1000, 
+
+                        'minWidth' => 1200, 
+                        'minHeight' => 600, 
+
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                            'image/jpg',
+                            'image/webp',
+                        ],
+                    ]),
+                ],
+                
+            ])
+
+            ->add('altDescription', TextType::class, [
+                'label' => 'Little description ',
+                'required' => true, 
+                'invalid_message' => 'The description must not exceed 125 characters.',
+                'constraints' => [
+                    new Length([
+                        'max' => 125,
+                        
+                    ]),
+                ],
+            ])
+
+            ->add('title', TextType::class, [
+                'label' => 'Title ',
+                'required' => false, 
+                'invalid_message' => 'The title must be between 5 and 30 characters.',
+                'constraints' => [
+                    new Length([
+                        'min' => 5, 
+                        'max' => 30, 
+                    ]),
+                ],
+            ])
+
         ;
     }
 
@@ -27,3 +77,5 @@ class PictureFormType extends AbstractType
         ]);
     }
 }
+
+

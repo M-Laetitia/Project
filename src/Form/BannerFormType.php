7cfe97dca7cd13a6dc868e7d5fd2088b6f0d1451
M-Laetitia <molin.laetitia@gmail.com>
2024-02-15 23:49:04 +0100

    Refactor: add banner form type to avoid conflict on the manage artist profil page

---
+++ src/Form/BannerFormType.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Picture;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Image;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class BannerFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+
+          ->add('picture', FileType::class, [
+                'label' => false,
+                // 'mapped' => false,
+                'required' => true, 
+                'property_path' => 'path', // le champ "picture" dans le form est lié à la propriété "path" de l'entité Picture
+                'invalid_message' => 'The image must have a maximum width of 1800 and a minimum of 1600, a maximum height of 1000 and a minimum of 600, its size must not exceed 2M, and the accepted formats are png, jpeg, jpg, and webp.',
+                'constraints' => [
+                    new Image([
+                        // 'maxSize' => '2M', 
+                        // 'maxWidth' => 1800, 
+                        // 'maxHeight' => 1000, 
+
+                        // 'minWidth' => 1200, 
+                        // 'minHeight' => 600, 
+
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                            'image/jpg',
+                            'image/webp',
+                        ],
+                    ]),
+                ],
+                
+            ])
+
+            ->add('altDescription', TextType::class, [
+                'label' => 'Little description ',
+                'required' => true, 
+                'invalid_message' => 'The description must not exceed 125 characters.',
+                'constraints' => [
+                    new Length([
+                        'max' => 125,
+                        
+                    ]),
+                ],
+            ])
+
+            ->add('title', TextType::class, [
+                'label' => 'Title ',
+                'required' => false, 
+                'invalid_message' => 'The title must be between 5 and 30 characters.',
+                'constraints' => [
+                    new Length([
+                        'min' => 5, 
+                        'max' => 30, 
+                    ]),
+                ],
+            ])
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Picture::class,
+        ]);
+    }
+}
+
+

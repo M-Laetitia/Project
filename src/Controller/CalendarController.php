2684e5a0fe615c1f81554c46eb66cb3670a99000
M-Laetitia <molin.laetitia@gmail.com>
2024-02-16 01:09:59 +0100

    Feat: get events/workshops/exhibitions to display on the global calendar

--- src/Controller/CalendarController.php
+++ src/Controller/CalendarController.php
@@ -2,17 +2,83 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\AreaRepository;
+use App\Repository\WorkshopRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CalendarController extends AbstractController
 {
     #[Route('/calendar', name: 'app_calendar')]
-    public function index(): Response
+    public function index(AreaRepository $areaRepository,  WorkshopRepository $workshopRepository): Response
     {
+
+        $formattedEvents = []; // formater les events pour les rendre compatibles avec FullCalendar
+
+        // ^ events 
+        $ongoingEvents = $areaRepository->findBy([
+            'type' => 'EVENT',
+            'status' => ['OPEN', 'PENDING', 'CLOSED'],
+        ]);
+        $pastEvents = $areaRepository->findBy([
+            'type' => 'EVENT',
+            'status' => ['ARCHIVED'],
+        ]);
+
+        foreach ($ongoingEvents as $event) {
+            $formattedEvents[] = [
+                'id' => $event->getId(),
+                'title' => $event->getName(),
+                'start' => $event->getStartDate()->format('Y-m-d H:i:s'),
+                'end' => $event->getEndDate()->format('Y-m-d H:i:s'),
+                'slug' => $event->getSlug(),
+            ];
+        }
+
+        // ^ expositions
+        $ongoingExpo = $areaRepository->findBy([
+            'type' => 'EXPO',
+            'status' => ['OPEN', 'PENDING', 'CLOSED'],
+        ]);
+        $pastExpo = $areaRepository->findBy([
+            'type' => 'EXPO',
+            'status' => ['ARCHIVED'],
+        ]);
+
+        foreach ($ongoingExpo as $expo) {
+            $formattedEvents[] = [
+                'id' => $expo->getId(),
+                'title' => $expo->getName(),
+                'start' => $expo->getStartDate()->format('Y-m-d H:i:s'),
+                'end' => $expo->getEndDate()->format('Y-m-d H:i:s'),
+                'slug' => $expo->getSlug(),
+            ];
+        }
+
+        // ^ workshop
+        $ongoingWorkshop = $workshopRepository->findBy([
+            'status' => ['OPEN', 'PENDING', 'CLOSED'],
+        ]);
+        $pastWorkshop = $workshopRepository->findBy([
+            'status' => ['ARCHIVED'],
+        ]);
+
+        foreach ($ongoingWorkshop as $workshop) {
+            $formattedEvents[] = [
+                'id' => $workshop->getId(),
+                'title' => $workshop->getName(),
+                'start' => $workshop->getStartDate()->format('Y-m-d H:i:s'),
+                'end' => $workshop->getEndDate()->format('Y-m-d H:i:s'),
+                'slug' => $workshop->getSlug(),
+            ];
+        }
+
+        // dd($formattedEvents);
+        
         return $this->render('calendar/index.html.twig', [
-            'controller_name' => 'CalendarController',
+            'formattedEvents' => json_encode($formattedEvents),
+
         ]);
     }
 }

a6b7076453702875194a2054fb36ed023efbcf6f
M-Laetitia <molin.laetitia@gmail.com>
2024-03-04 00:52:56 +0100

    google auth  - variable

---
+++ config/config.php
@@ -0,0 +1,4 @@
+<?php
+// variable d'environnement 
+define('GOOGLE_ID', '346078539825-0uda84aeorj9gb7jindava1pdct869qm.apps.googleusercontent.com');
+define('GOOGLE_SECRET',  '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' );

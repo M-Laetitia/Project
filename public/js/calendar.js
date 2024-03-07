accf87e37a2bea485a0c368d007ee9e45c78abe9
M-laetitia <molin.laetitia@gmail.com>
2024-03-07 16:14:05 +0100

    Style: calendar page styling (wip)

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,28 +1,23 @@
 
-
     document.addEventListener('DOMContentLoaded', function() {
         const calendarEl = document.getElementById('calendar');
         
-        // Insérez ici le code pour récupérer les données du serveur
         
-
         // Récupérer la chaîne JSON de l'attribut data-formatted-events
-  const formattedEventsString = document.getElementById('calendar').getAttribute('data-formatted-events');
+        const formattedEventsString = document.getElementById('calendar').getAttribute('data-formatted-events');
 
-  // récupérer sous forme de tableau:
-  const formattedEvents = JSON.parse(formattedEventsString);
+        // récupérer sous forme de tableau:
+        const formattedEvents = JSON.parse(formattedEventsString);
 
-  // formatter les dates 
-  formattedEvents.forEach(event => {
-      event.start = new Date(event.start);
-      event.end = new Date(event.end);
-      event.id = event.slug;
-      console.log(event.end)
-      console.log(event.end.toISOString());
-    });
+        // formatter les dates 
+        formattedEvents.forEach(event => {
+            event.start = new Date(event.start);
+            event.end = new Date(event.end);
+            event.id = event.slug;
+            console.log(event.end)
+            console.log(event.end.toISOString());
+            });
     
-
-        
         const calendar = new FullCalendar.Calendar(calendarEl, {
             initialView: 'dayGridMonth',
             events: formattedEvents,
@@ -31,8 +26,17 @@
                 console.log('slug', info.event.id)
                 window.location.href = '/event/' + info.event.id;
             },
-            timeZone: 'UTC', // ou 'UTC' ou le fuseau horaire souhaité
+            timeZone: 'UTC',
             //displayEventEnd: true, // Affiche l'heure de fin
+
+            // Hide top header bar
+            header: {
+                left: '',
+                center: '',
+                right: ''
+            },
+
+            
 
             ///eventContent: function(arg) {
                 //return {}; 
@@ -45,16 +49,6 @@
        
         });
         
-        calendar.render();
-    });
+    calendar.render();
+});
 
-
-
-
-//   var calendar = new Calendar(calendarEl, {
-//     plugins: [ dayGridPlugin, interactionPlugin ],
-//     events: '/api/events', // Remplacez cette URL par le chemin de votre API Symfony qui renvoie les événements
-//     eventClick: function(info) {
-//         window.location.href = '/event/' + info.event.id;
-//     }
-// });

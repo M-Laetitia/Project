be9e48d0d6d7fc89f95ced2888b6a5b1192246b7
M-Laetitia <molin.laetitia@gmail.com>
2024-03-20 13:51:41 +0100

    Refactor: add conditionto check if the error is a denied access and redirect to the 403 custom page

--- src/Controller/ErrorController.php
+++ src/Controller/ErrorController.php
@@ -2,14 +2,26 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Throwable;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class ErrorController extends AbstractController
 {
-    public function show(): Response
+    public function show(Throwable $exception): Response
     {
+        // Check if the exception is an AccessDeniedException
+        if ($exception instanceof AccessDeniedHttpException) {
+            return $this->render('bundles/TwigBundle/Exception/error403.html.twig');
+        }
+        
+        // For non-AccessDeniedException exceptions, render a 404 error page
         return $this->render('bundles/TwigBundle/Exception/error404.html.twig');
     }
 }
+
+
+

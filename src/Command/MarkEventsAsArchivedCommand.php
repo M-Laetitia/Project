4487fc01996a2776f03444a7f82557e055b703fe
M-Laetitia <molin.laetitia@gmail.com>
2024-03-30 20:43:10 +0100

    Refactor: add english comments

--- src/Command/MarkEventsAsArchivedCommand.php
+++ src/Command/MarkEventsAsArchivedCommand.php
@@ -13,52 +13,44 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class MarkEventsAsArchivedCommand extends Command 
 {
-    private $entityManager;
-    private $areaRepository;
-    private $workshopRepository;
+    private $entityManager; // EntityManager instance for managing entities in the db
+    private $areaRepository; // Repository for accessing Area entities in the database
+    private $workshopRepository; // Same for Workshop
     
     public function __construct(EntityManagerInterface $entityManager, AreaRepository $areaRepository, WorkshopRepository $workshopRepository) {
 
         parent::__construct();
-
         $this->entityManager = $entityManager;
         $this->areaRepository = $areaRepository;
         $this->workshopRepository = $workshopRepository;
 
     }
-
     protected function configure()
     {
-        $this->setName('app:mark-event-as-archived')
-            ->setDescription('Mark event as archived');
+        $this->setName('app:mark-event-as-archived')  // Set the name of the command
+            ->setDescription('Mark event as archived'); // Set the description of the command
     }
-
     protected  function execute(InputInterface $input, OutputInterface $output) {
-
-        $currentDate = new \DateTime();
-
-
+        $currentDate = new \DateTime(); // Get the current date and time
         $areas = $this->areaRepository->findAll();
         $workshops = $this->workshopRepository->findAll();
 
-        // foreach ($areas as $area) {
-        //     $endDate= $area->getEndDate();
-        //     if ($endDate <= $currentDate) {
-        //         $area->setStatus("ARCHIVED");
-        //     }
+        // Iterate through each Area entity
+        foreach ($areas as $area) {
+            $endDate= $area->getEndDate();
+            // Check if the end date is before or equal to the current date
+            if ($endDate <= $currentDate) {
+                $area->setStatus("ARCHIVED"); // Set the status of the area to "ARCHIVED"
+            }
         
-        // }
-
+        }
         foreach ($workshops as $workshop) {
             $endDate= $workshop->getEndDate();
             if ($endDate <= $currentDate) {
                 $workshop->setStatus("ARCHIVED");
             }        
         }
-
-
         $this->entityManager->flush();
-
         return Command::SUCCESS;
     }
 }

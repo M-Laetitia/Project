e04e1437722f44b7f9638cd46b0366c762096abc
M-Laetitia <molin.laetitia@gmail.com>
2024-04-19 03:12:15 +0200

    Update: add unique for the name field

--- src/Entity/Area.php
+++ src/Entity/Area.php
@@ -17,7 +17,7 @@ class Area
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 100)]
+    #[ORM\Column(length: 100, unique: true)]
     private ?string $name = null;
 
     #[ORM\Column(length: 255)]
